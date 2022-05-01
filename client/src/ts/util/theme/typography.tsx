5890f0a0aaa37a549e5b899db76f29d3164690dd
Clay Dunston <claydunston@gmail.com>
2022-05-01 12:25:59 -0500

    [client] Replace font source

--- client/src/ts/util/theme/typography.tsx
+++ client/src/ts/util/theme/typography.tsx
@@ -3,73 +3,18 @@ import {
     Components,
 } from "@mui/material"
 
-// import AgmenaProWoff2 from "../../../assets/fonts/AgmenaPro/woff/AgmenaPro-Regular.woff2"
-import AgmenaProWoff2     from "@assets/fonts/AgmenaPro/woff/AgmenaPro-Regular.woff2"
-// import AgmenaProBoldWoff2 from "@assets/fonts/AgmenaPro/woff/AgmenaPro-Bold.woff2"
-import "../../../assets/fonts/AgmenaPro/woff/AgmenaPro-Regular.woff2"
+// // import AgmenaProWoff2 from "../../../assets/fonts/AgmenaPro/woff/AgmenaPro-Regular.woff2"
+// import AgmenaProWoff2     from "@assets/fonts/AgmenaPro/woff/AgmenaPro-Regular.woff2"
+// // import AgmenaProBoldWoff2 from "@assets/fonts/AgmenaPro/woff/AgmenaPro-Bold.woff2"
+// import "../../../assets/fonts/AgmenaPro/woff/AgmenaPro-Regular.woff2"
 
-const fontFolderPath = "../assets/fonts/AgmenaPro/woff/"
-
-const cssString = /*CSS*/`
-    @font-face {
-        font-family: 'Agmena Pro';
-        src: url('../assets/fonts/AgmenaPro/woff/AgmenaPro-Bold.woff2') format('woff2'),
-             url('../assets/fonts/AgmenaPro/woff/AgmenaPro-Bold.woff') format('woff');
-        font-weight: bold;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'Agmena Pro';
-        src: url('../assets/fonts/AgmenaPro/woff/AgmenaPro-BoldItalic.woff2') format('woff2'),
-             url('../assets/fonts/AgmenaPro/woff/AgmenaPro-BoldItalic.woff') format('woff');
-        font-weight: bold;
-        font-style: italic;
-    }
-
-    @font-face {
-        font-family: 'Agmena Pro Book';
-        src: url('../assets/fonts/AgmenaPro/woff/AgmenaPro-Book.woff2') format('woff2'),
-             url('../assets/fonts/AgmenaPro/woff/AgmenaPro-Book.woff') format('woff');
-        font-weight: 300;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'Agmena Pro Book';
-        src: url('../assets/fonts/AgmenaPro/woff/AgmenaPro-BookItalic.woff2') format('woff2'),
-             url('../assets/fonts/AgmenaPro/woff/AgmenaPro-BookItalic.woff') format('woff');
-        font-weight: 300;
-        font-style: italic;
-    }
-
-    @font-face {
-        font-family: 'Agmena Pro';
-        src: url('../assets/fonts/AgmenaPro/woff/AgmenaPro-Regular.woff2') format('woff2'),
-            url('../assets/fonts/AgmenaPro/woff/AgmenaPro-Regular.woff') format('woff');
-        font-weight: normal;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'Agmena Pro';
-        src: url('../assets/fonts/AgmenaPro/woff/AgmenaPro-Italic.woff2') format('woff2'),
-            url('../assets/fonts/AgmenaPro/woff/AgmenaPro-Italic.woff') format('woff');
-        font-weight: normal;
-        font-style: italic;
-    }
-
-    * {
-        /* font-family: 'Agmena Pro', serif !important; */
-        font-family: 'Agmena Pro', serif;
-    }
-
-`
+// const fontFolderPath = "../assets/fonts/AgmenaPro/woff/"
 
 export const typography: TypographyVariantsOptions = {
     // allVariants: {
     fontFamily: [
-        "Agmena Pro",
+        "garamond-premier-pro-display",
+        // "Agmena Pro",
         // '"Helvetica Neue"',
         // "Arial",
         // "sans-serif",
@@ -77,16 +22,17 @@ export const typography: TypographyVariantsOptions = {
     // },
 }
 
+const baseTypographyStyles = `
+    @font-face {
+        font-family: garamond-premier-pro-display;
+        font-style: normal;
+        font-display: swap;
+        font-weight: 400;
+    }
+`
+
 export const typographyComponents: Components = {
     MuiCssBaseline: {
-        styleOverrides: `
-            @font-face {
-                font-family: 'Agmena Pro';
-                font-style: normal;
-                font-display: swap;
-                font-weight: 400;
-                src: local('Agmena Pro'), url(${AgmenaProWoff2}) format('woff2');
-            }
-          `,
+        styleOverrides: baseTypographyStyles,
     },
 }
