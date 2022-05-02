15ab06dbd9f6a945d3b2c3f15659dad500563163
Clay Dunston <claydunston@gmail.com>
2022-05-02 16:37:39 -0500

    [FMT]

--- server/db/migrate/20220502211144_create_weapon_affinities.rb
+++ server/db/migrate/20220502211144_create_weapon_affinities.rb
@@ -1,5 +1,5 @@
 class CreateWeaponAffinities < ActiveRecord::Migration[7.0]
-  def change
+  def change()
     create_table(:weapon_affinities) do |t|
       t.string(:name, null: false, index: { unique: true })
       t.jsonb(:metadata, default: {})
