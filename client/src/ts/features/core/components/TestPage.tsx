6ee673294243b6028c85d78ad8bcf4a1e63a54d6
Clay Dunston <claydunston@gmail.com>
2022-05-07 16:16:55 -0500

    [client] [WIP] popover border styles

--- client/src/ts/features/core/components/TestPage.tsx
+++ client/src/ts/features/core/components/TestPage.tsx
@@ -19,19 +19,39 @@ const boxStyle = {
 
 export const TestPage = (): JSX.Element => {
 
+    const className = [
+        "bg-success",
+        "h-100",
+        "w-100",
+        // "flex-center",
+        // "flex-col",
+        "flex-between-column",
+        "p-5",
+    ].join(" ")
+
     return (
         <>
-            <Heading title="Test" />
-            <section className="bg-success h-100 w-100 flex-stretch-row">
-                {/* <Link to="/" className="btn btn-primary">Home</Link> */}
-                {/* <TalismanDisplay talisman={t} /> */}
-                {/* <ECard
-                    title="Attack Power"
-                    iconPath={mdiArmFlexOutline}
-                /> */}
-                <div className="bg-info"    style={boxStyle}>uno</div>
-                <div className="bg-warning" style={boxStyle}>dos</div>
-                <div className="bg-danger"  style={boxStyle}>tres</div>
+            {/* <Heading title="Test" /> */}
+            <section className={className}>
+                <div></div>
+
+                <div className="er-popover">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, itaque nostrum! Numquam excepturi officia aliquid quasi nihil velit eum cum vero nesciunt inventore rem rerum asperiores corrupti, accusantium facilis voluptatibus.
+                </div>
+
+                <div className="er-popover-2">
+                    <div className="er-popover-border er-popover-border-top">&nbsp;</div>
+                    <div className="er-popover-border er-popover-border-bottom">&nbsp;</div>
+                    <div className="er-popover-border er-popover-border-left">&nbsp;</div>
+                    <div className="er-popover-border er-popover-border-right">&nbsp;</div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, itaque nostrum! Numquam excepturi officia aliquid quasi nihil velit eum cum vero nesciunt inventore rem rerum asperiores corrupti, accusantium facilis voluptatibus.
+                </div>
+
+                <div className="er-popover-3">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, itaque nostrum! Numquam excepturi officia aliquid quasi nihil velit eum cum vero nesciunt inventore rem rerum asperiores corrupti, accusantium facilis voluptatibus.
+                </div>
+
+                <div></div>
             </section>
         </>
     )
