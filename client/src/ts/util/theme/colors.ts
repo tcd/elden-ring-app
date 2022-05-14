547187d335c9ad0ae45e708418184e41066d18e4
Clay Dunston <claydunston@gmail.com>
2022-05-14 12:40:27 -0500

    [client] [FMT]

--- client/src/ts/util/theme/colors.ts
+++ client/src/ts/util/theme/colors.ts
@@ -1,9 +1,10 @@
 export const COLORS = {
-    black: "#292927",
-    white: "#D6CDB8",
-    red:   "#CD3138",
-    yellow: "#B6AD84",
-    green: "#608b4e",
-    blue:  "#527ED5",
+    black:  "#292927",
+    white:  "#d6cdb8",
+    red:    "#cd3138",  // good
+    yellow: "#b6ad84",
+    green:  "#608b4e",
+    blue:   "#527ed5",  // good
+    purple: "#c586c0",
+    cyan:   "#4ec9b0",
 }
-
