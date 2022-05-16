1907a8d327eae2ed3156fdbb8827ad753ec09e1d
Clay Dunston <claydunston@gmail.com>
2022-05-15 19:43:24 -0500

    [WIP] DamageTypes interface

---
+++ client/src/ts/types/DamageTypes.ts
@@ -0,0 +1,19 @@
+export interface DamageTypes {
+    inflicts_physical: boolean
+    // _inflicts_strike?: boolean
+    // _inflicts_slash?: boolean
+    // _inflicts_pierce?: boolean
+    inflicts_magic: boolean
+    inflicts_fire: boolean
+    inflicts_lightning: boolean
+    inflicts_holy: boolean
+    inflicts_blood_loss: boolean
+    inflicts_frost: boolean
+    inflicts_madness: boolean
+    inflicts_poison: boolean
+    inflicts_sleep: boolean
+    inflicts_death_blight: boolean
+    inflicts_scarlet_rot: boolean
+    // _inflicts_destined_death?: boolean
+    // _inflicts_black_flame?: boolean
+}
