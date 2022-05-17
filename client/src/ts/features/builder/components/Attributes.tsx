8dbdc35f6e609ea2cd42c8a85aafab9c20be37be
Clay Dunston <claydunston@gmail.com>
2022-05-16 20:13:34 -0500

    [client] Improve attribute leveling elements

--- client/src/ts/features/builder/components/Attributes.tsx
+++ client/src/ts/features/builder/components/Attributes.tsx
@@ -8,44 +8,75 @@ import { AttributeNames } from "@types"
 import { Actions, Selectors } from "@app/state"
 import { ECard } from "@app/shared"
 
-export const Attributes = (): JSX.Element => {
+export interface AttributeRowProps {
+    name: string
+    value: number
+}
+
+export const AttributeRow = ({ name, value }: AttributeRowProps): JSX.Element => {
 
     const dispatch = useDispatch()
-    const attributes = useSelector(Selectors.Builder.attributes)
 
-    const incrementAttribute = (name: string) => {
-        dispatch(Actions.Builder.incrementAttribute({ name }))
+    const incrementAttribute = () => {
+        dispatch(Actions.Builder.incrementAttribute({ name: name.toLowerCase() }))
     }
 
-    const decrementAttribute = (name: string) => {
-        dispatch(Actions.Builder.decrementAttribute({ name }))
+    const decrementAttribute = () => {
+        dispatch(Actions.Builder.decrementAttribute({ name: name.toLowerCase() }))
     }
 
-    const row = (name: string) => {
-        return (
-            <li className="attribute-row" key={`attribute_${name}`}>
-                <span className="text-capitalize">{name}</span>
-                <div className="attribute-control">
-                    <IconButton size="small" onClick={() => decrementAttribute(name)}>
-                        <ArrowBackIosNewIcon />
-                    </IconButton>
-                    <span className="attribute-value">
-                        {attributes[name]}
-                    </span>
-                    <IconButton size="small" onClick={() => incrementAttribute(name)}>
-                        <ArrowForwardIosIcon />
-                    </IconButton>
-                </div>
-            </li>
-        )
-    }
+    return (
+        <li className="attribute-row">
+            <span className="text-capitalize">{name}</span>
+            <div className="attribute-control">
+                <IconButton size="small" onClick={decrementAttribute}>
+                    <ArrowBackIosNewIcon />
+                </IconButton>
+                <span className="attribute-value">
+                    {value}
+                </span>
+                <IconButton size="small" onClick={incrementAttribute}>
+                    <ArrowForwardIosIcon />
+                </IconButton>
+            </div>
+        </li>
+    )
+}
 
-    const rows = AttributeNames.map(attr => row(attr))
+export const Attributes = (): JSX.Element => {
+
+    const vigor        = useSelector(Selectors.Builder.attribute.vigor)
+    const mind         = useSelector(Selectors.Builder.attribute.mind)
+    const endurance    = useSelector(Selectors.Builder.attribute.endurance)
+    const strength     = useSelector(Selectors.Builder.attribute.strength)
+    const dexterity    = useSelector(Selectors.Builder.attribute.dexterity)
+    const intelligence = useSelector(Selectors.Builder.attribute.intelligence)
+    const faith        = useSelector(Selectors.Builder.attribute.faith)
+    const arcane       = useSelector(Selectors.Builder.attribute.arcane)
+
+    const attributeData: AttributeRowProps[] = [
+        { name: "Vigor",        value: vigor        },
+        { name: "Mind",         value: mind         },
+        { name: "Endurance",    value: endurance    },
+        { name: "Strength",     value: strength     },
+        { name: "Dexterity",    value: dexterity    },
+        { name: "Intelligence", value: intelligence },
+        { name: "Faith",        value: faith        },
+        { name: "Arcane",       value: arcane       },
+    ]
+
+    const attributeRows = attributeData.map(({ name, value }) => {
+        return <AttributeRow
+            name={name}
+            value={value}
+            key={`attribute-leveling-row-${name}`}
+        />
+    })
 
     return (
         <ECard title="Attribute Points" iconPath={mdiArmFlex}>
             <ul>
-                {rows}
+                {attributeRows}
             </ul>
         </ECard>
     )
