e8025876d366f9df67fe7f6e50b972aeb2a53657
Clay Dunston <claydunston@gmail.com>
2022-05-24 14:08:03 -0500

    [client] [WIP] About page

--- client/src/ts/features/core/components/pages/AboutPage.tsx
+++ client/src/ts/features/core/components/pages/AboutPage.tsx
@@ -4,6 +4,14 @@ export const AboutPage = (): JSX.Element => {
     return (
         <>
             <Heading title="About" />
+            <section>
+                <h3>Thanks, Credit, &amp; Recognition:</h3>
+                <ul>
+                    <li>
+                        All Weapon Damage calculations are from <a href="https://www.reddit.com/user/TarnishedSpreadsheet/">u/TarnishedSpreadsheet</a>&apos; <a href="https://www.reddit.com/r/Eldenring/comments/tbco46/elden_ring_weapon_calculator/">Elden Ring Weapon Calculator</a>.
+                    </li>
+                </ul>
+            </section>
         </>
     )
 }
