ed57a7318a31e0dfd2c76b056c3754071dd2297f
Clay Dunston <claydunston@gmail.com>
2022-06-05 10:29:02 -0500

    [client] Delete modal code

--- client/src/ts/features/builder/components/BuilderPage.tsx
+++ client/src/ts/features/builder/components/BuilderPage.tsx
@@ -5,20 +5,14 @@ import {
     Attributes,
     Overview,
 } from "./level-up"
-import { Equipment } from "./equipment"
-import { ActionMenu } from "./action-menu"
-import { CharacterStatus } from "./right-side-panels"
-import {
-    ArmorModal,
-    TalismanModal,
-    WeaponModal,
-} from "./modals"
-
 import {
     ArmorMenu,
     TalismanMenu,
     WeaponMenu,
 } from "@app/features"
+import { CharacterStatus } from "./right-side-panels"
+import { ActionMenu } from "./action-menu"
+import { Equipment } from "./equipment"
 
 export const BuilderPage = (): JSX.Element => {
 
