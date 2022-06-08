acd5427200d2311773f2a4a559ecf7dcf3e732be
Clay Dunston <claydunston@gmail.com>
2022-06-08 18:00:38 -0500

    [client] Update static data

---
+++ client/src/ts/data/weapon-affinities.ts
@@ -0,0 +1,19 @@
+import {
+    WeaponAffinity,
+} from "@app/types"
+
+export const WEAPON_AFFINITIES: WeaponAffinity[] = [
+    { "id":  1, "name": "Standard",  "display_name": "Standard"  },
+    { "id":  2, "name": "Heavy",     "display_name": "Heavy"     },
+    { "id":  3, "name": "Keen",      "display_name": "Keen"      },
+    { "id":  4, "name": "Quality",   "display_name": "Quality"   },
+    { "id":  5, "name": "Fire",      "display_name": "Fire"      },
+    { "id":  6, "name": "Flame Art", "display_name": "Flame Art" },
+    { "id":  7, "name": "Lightning", "display_name": "Lightning" },
+    { "id":  8, "name": "Sacred",    "display_name": "Sacred"    },
+    { "id":  9, "name": "Magic",     "display_name": "Magic"     },
+    { "id": 10, "name": "Cold",      "display_name": "Cold"      },
+    { "id": 11, "name": "Poison",    "display_name": "Poison"    },
+    { "id": 12, "name": "Blood",     "display_name": "Blood"     },
+    { "id": 13, "name": "Occult",    "display_name": "Occult"    },
+]
