f083331f313dcc630dd244a4c9b8a356747503b6
Clay Dunston <claydunston@gmail.com>
2022-06-14 14:13:54 -0500

    [Client] Work on defense

--- client/src/ts/util/calculations/defense/magic-defense-from-intelligence.ts
+++ client/src/ts/util/calculations/defense/magic-defense-from-intelligence.ts
@@ -1,17 +1,17 @@
 // Level 1 - 20
-const formula_1 = (level: number) => (     40 *  (level       / 20))
+const formula_1 = (level: Integer): Decimal => ( 0 + 40 * ((level -  0) / 20))
 // Level 11 - 35
-const formula_2 = (level: number) => (40 + 10 * ((level - 20) / 15))
+const formula_2 = (level: Integer): Decimal => (40 + 10 * ((level - 20) / 15))
 // Level 36 - 60
-const formula_3 = (level: number) => (50 + 10 * ((level - 35) / 25))
+const formula_3 = (level: Integer): Decimal => (50 + 10 * ((level - 35) / 25))
 // Level 61 - 99
-const formula_4 = (level: number) => (60 + 10 * ((level - 60) / 39))
+const formula_4 = (level: Integer): Decimal => (60 + 10 * ((level - 60) / 39))
 
 /**
  * Based on formulas from the [Fextralife Wiki](https://eldenring.wiki.fextralife.com/Magic+Defense)
  * @param intLevel Intelligence Level
  */
-export const magicDefenseFromIntelligence = (intLevel: number) => {
+export const magicDefenseFromIntelligence = (intLevel: Integer): Decimal => {
     if (intLevel <= 20) { return formula_1(intLevel) }
     if (intLevel <= 35) { return formula_2(intLevel) }
     if (intLevel <= 60) { return formula_3(intLevel) }
