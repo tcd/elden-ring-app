7bb9c90bcf661532f398ce139f0ebf3fb92b47ef
Clay Dunston <claydunston@gmail.com>
2022-06-15 19:16:32 -0500

    [Server] Add `Lib::Constants::CutContent`

---
+++ server/app/lib/lib/constants/cut_content.rb
@@ -0,0 +1,30 @@
+module Lib
+  module Constants
+    module CutContent
+
+      # @return [Array<String>]
+      ARMOR = [
+        "Brave's Battlewear (Altered)",
+        "Brave's Battlewear",
+        "Brave's Bracer",
+        "Brave's Cord Circlet",
+        "Brave's Leather Helm",
+        "Brave's Legwraps",
+        "Deathbed Smalls",
+        "Golden Prosthetic",
+        "Grass Hair Ornament",
+        "Millicent's Boots",
+        "Millicent's Gloves",
+        "Millicent's Robe",
+        "Millicent's Tunic",
+        "Ragged Armor (Altered)",
+        "Ragged Armor",
+        "Ragged Gloves",
+        "Ragged Hat (Altered)",
+        "Ragged Hat",
+        "Ragged Loincloth",
+      ].freeze()
+
+    end
+  end
+end
