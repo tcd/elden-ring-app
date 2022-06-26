dfa899911ad612d0bc55b99a53a998a5cae054f3
Clay Dunston <claydunston@gmail.com>
2022-06-26 12:00:30 -0500

    [Server] Update serializer tests

--- server/test/serializers/equipment_effect_test.rb
+++ server/test/serializers/equipment_effect_test.rb
@@ -12,8 +12,14 @@ module Test
       test ".render_as_json" do
         entity = @model.first()
         data = @serializer.render_as_json(entity)
-        # refute_nil(entity.display_name)
-        refute_nil(data["display_name"])
+        refute_nil(entity.effect_type)
+        refute_nil(entity.effect_method)
+        refute_nil(entity.affected_stat)
+        refute_nil(entity.value)
+        refute_nil(data["effect_type"])
+        refute_nil(data["effect_method"])
+        refute_nil(data["affected_stat"])
+        refute_nil(data["value"])
       end
 
     end
--- server/test/serializers/weapon_affinity_test.rb
+++ server/test/serializers/weapon_affinity_test.rb
@@ -12,8 +12,8 @@ module Test
       test ".render_as_json" do
         entity = @model.first()
         data = @serializer.render_as_json(entity)
-        # refute_nil(entity.display_name)
-        refute_nil(data["display_name"])
+        refute_nil(entity.name)
+        refute_nil(data["name"])
       end
 
     end
