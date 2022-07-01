e2ba77faca924512566e12a179f7f4e7fa3b318c
Clay Dunston <claydunston@gmail.com>
2022-07-01 10:13:08 -0500

    [Client] Rename param for `meetsRequirements`

--- client/src/ts/util/meets-requirements.ts
+++ client/src/ts/util/meets-requirements.ts
@@ -4,13 +4,13 @@ import {
     Weapon,
 } from "@app/types"
 
-export const meetsRequirements = (stats: Attributes, equipment: Weapon | Spell): boolean => {
+export const meetsRequirements = (attributes: Attributes, equipment: Weapon | Spell): boolean => {
     // @ts-ignore:next-line
-    if (stats.strength     < (equipment?.required_strength     ?? 0)) { return false }
+    if (attributes.strength     < (equipment?.required_strength     ?? 0)) { return false }
     // @ts-ignore:next-line
-    if (stats.dexterity    < (equipment?.required_dexterity    ?? 0)) { return false }
-    if (stats.intelligence < (equipment?.required_intelligence ?? 0)) { return false }
-    if (stats.faith        < (equipment?.required_faith        ?? 0)) { return false }
-    if (stats.arcane       < (equipment?.required_arcane       ?? 0)) { return false }
+    if (attributes.dexterity    < (equipment?.required_dexterity    ?? 0)) { return false }
+    if (attributes.intelligence < (equipment?.required_intelligence ?? 0)) { return false }
+    if (attributes.faith        < (equipment?.required_faith        ?? 0)) { return false }
+    if (attributes.arcane       < (equipment?.required_arcane       ?? 0)) { return false }
     return true
 }
