b2d378fbf2e9b730f243db5cfeba2031ecd5379e
Clay Dunston <claydunston@gmail.com>
2022-07-05 18:41:33 -0500

    [client] idk

--- client/src/ts/app/AppRoot.tsx
+++ client/src/ts/app/AppRoot.tsx
@@ -1,28 +1,16 @@
 import { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { BrowserRouter as Router } from "react-router-dom"
-import { CssBaseline, ThemeProvider, SxProps } from "@mui/material"
-import Backdrop from "@mui/material/Backdrop"
-import CircularProgress from "@mui/material/CircularProgress"
+import { CssBaseline, ThemeProvider } from "@mui/material"
 
-import {
-    EldenRingMaterialThemeDark,
-    // EldenRingMaterialThemeLight,
-} from "@app/util"
+import { EldenRingMaterialThemeDark, isBlank } from "@app/util"
 import { Actions, Selectors } from "@app/state"
 import { AppRoutesComponent } from "./AppRoutesComponent"
-
-const backdropSx: SxProps = {
-    color: "#fff",
-    // zIndex: (theme) => theme.zIndex.drawer + 1
-    zIndex: 50,
-}
 
 export const AppRoot = (_props: unknown): JSX.Element => {
 
     const dispatch = useDispatch()
 
-    // const darkModeEnabled = useSelector(Selectors.Core.darkModeEnabled)
     // const fetching: boolean = useSelector(Selectors.Builder.api.loading)
     // const shouldFetch: boolean = useSelector(Selectors.Builder.api.shouldFetchEverything)
     // const fetchFailed: boolean = useSelector(Selectors.Builder.api.fetchFailed)
@@ -33,7 +21,7 @@ export const AppRoot = (_props: unknown): JSX.Element => {
 
     // useEffect(() => {
     //     if (shouldFetch) { dispatch(Actions.Builder.fetchEverything()) }
-    // })
+    // }, [shouldFetch])
 
     // if (fetchFailed) {
     //     return (
@@ -57,10 +45,6 @@ export const AppRoot = (_props: unknown): JSX.Element => {
         <ThemeProvider theme={theme}>
             <CssBaseline />
             {_mainContent}
-            {/* {mainContent}
-            <Backdrop id="er__loadingBackdrop" open={fetching}>
-                <CircularProgress color="inherit" />
-            </Backdrop> */}
         </ThemeProvider>
     )
 }
