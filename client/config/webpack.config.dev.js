664dc7ab98f6fc984a7516483745acefe1e4c78e
Clay Dunston <claydunston@gmail.com>
2022-07-04 19:19:45 -0500

    [Client] update webpack config

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -1,11 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const path = require("path")
-const webpack = require("webpack")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
-const dotEnv = require("dotenv-webpack")
-
+const shared = require("./webpack.config.shared")
 const rootFolder = path.resolve(__dirname, "..")
 
 /**
@@ -14,13 +10,8 @@ const rootFolder = path.resolve(__dirname, "..")
  * @type {import("webpack").Configuration}
  */
 const webpackConfig = {
+    ...shared,
     mode: "development",
-    entry: "./src/index.tsx",
-    output: {
-        path: path.resolve(rootFolder, "dist"),
-        publicPath: "/",
-        filename: "bundle.js",
-    },
     devtool: "inline-source-map",
     /** @type {import("webpack").Configuration} */
     devServer: {
@@ -37,65 +28,6 @@ const webpackConfig = {
         port: 8080,
         allowedHosts: "all",
         hot: true,
-    },
-    plugins: [
-        new dotEnv(),
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            hash: true,
-            inject: true,
-        }),
-        new webpack.ProvidePlugin({ process: "process/browser" }),
-    ],
-    resolve: {
-        extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
-        modules: [rootFolder, "src", "node_modules"],
-        fallback: {
-            console: false,
-        },
-        plugins: [
-            new TsconfigPathsPlugin(),
-        ],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/i,
-                exclude: /node_modules/,
-                use: ["babel-loader"],
-            },
-            {
-                enforce: "pre",
-                test: /\.tsx?$/,
-                use: "source-map-loader",
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                type: "asset/resource",
-                generator: {
-                    filename: "fonts/[name][ext][query]",
-                },
-                // use: [{
-                //     loader: "file-loader",
-                //     options: {
-                //         name: "[name].[ext]",
-                //         outputPath: "fonts/",
-                //     },
-                // }],
-            },
-            {
-                test: /\.png/,
-                type: "asset/resource",
-            },
-        ],
     },
 }
 
--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -1,12 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const path = require("path")
-const webpack = require("webpack")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
-const dotEnv = require("dotenv-webpack")
-
-const rootFolder = path.resolve(__dirname, "..")
+const shared = require("./webpack.config.shared")
 
 /**
  * See [Webpack Configuration docs](https://webpack.js.org/configuration/) for more information.
@@ -14,73 +8,8 @@ const rootFolder = path.resolve(__dirname, "..")
  * @type {import("webpack").Configuration}
  */
 const webpackConfig = {
+    ...shared,
     mode: "production",
-    entry: "./src/index.tsx",
-    output: {
-        path: path.resolve(rootFolder, "dist"),
-        publicPath: "/",
-        filename: "bundle.js",
-    },
-    devtool: "inline-source-map",
-    plugins: [
-        new dotEnv(),
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            hash: true,
-            inject: true,
-        }),
-        new webpack.ProvidePlugin({ process: "process/browser" }),
-    ],
-    resolve: {
-        extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
-        modules: [rootFolder, "src", "node_modules"],
-        fallback: {
-            console: false,
-        },
-        plugins: [
-            new TsconfigPathsPlugin(),
-        ],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/i,
-                exclude: /node_modules/,
-                use: ["babel-loader"],
-            },
-            {
-                enforce: "pre",
-                test: /\.tsx?$/,
-                use: "source-map-loader",
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                type: "asset/resource",
-                generator: {
-                    filename: "fonts/[name][ext][query]",
-                },
-                // use: [{
-                //     loader: "file-loader",
-                //     options: {
-                //         name: "[name].[ext]",
-                //         outputPath: "fonts/",
-                //     },
-                // }],
-            },
-            {
-                test: /\.png/,
-                type: "asset/resource",
-            },
-        ],
-    },
 }
 
 module.exports = webpackConfig
