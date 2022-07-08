c62a21f6b24eef59c0be26b48fef2e68b505bf59
Clay Dunston <claydunston@gmail.com>
2022-07-08 15:26:29 -0500

    [Client] Remove *starting-class-2* code

--- client/src/ts/features/builder/components/index.ts
+++ client/src/ts/features/builder/components/index.ts
@@ -1,4 +1,3 @@
 export * from "./BuilderPage"
 export * from "./MainBuilderMenu"
 export * from "./starting-class"
-export * from "./starting-class-2"
--- client/src/ts/features/builder/components/starting-class-2/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./StartingClassMenu2"
-export * from "./StartingClassPage2"
-export * from "./StartingClassEquipment2"
-export * from "./StartingClassOption2"
