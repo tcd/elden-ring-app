ef834ba7c42adcd256a78fabf9df10078e1883cc
Clay Dunston <claydunston@gmail.com>
2022-07-07 19:24:52 -0500

    [WIP] be more efficient

--- client/src/ts/data/kitchen-sink/index.ts
+++ client/src/ts/data/kitchen-sink/index.ts
@@ -1,2 +1,5 @@
-// import * as data from "./kitchen-sink.json"
+import { KitchenSink } from "@app/types"
+import * as data from "./kitchen-sink.json"
+
+export const KITCHEN_SINK_DATA: KitchenSink = data as unknown as KitchenSink
 
