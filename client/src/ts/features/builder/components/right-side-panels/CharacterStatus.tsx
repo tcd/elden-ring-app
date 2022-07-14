8afdf319744e98e296d2fef97a72d6e4ec6e3b17
Clay Dunston <claydunston@gmail.com>
2022-07-13 19:56:22 -0500

    [client] formatting

--- client/src/ts/features/builder/components/right-side-panels/CharacterStatus.tsx
+++ client/src/ts/features/builder/components/right-side-panels/CharacterStatus.tsx
@@ -10,13 +10,8 @@ export const CharacterStatus = (): JSX.Element => {
 
     const whichStatus = useSelector(Selectors.Builder.whichCharacterStatus)
 
-    if (whichStatus == "A") {
-        return (<CharacterStatusA />)
-    }
-
-    if (whichStatus == "B") {
-        return (<CharacterStatusB />)
-    }
+    if (whichStatus == "A") { return (<CharacterStatusA />) }
+    if (whichStatus == "B") { return (<CharacterStatusB />) }
 
     return null
 }
