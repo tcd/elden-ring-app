3b9738796ab07ceac3d52d9facfa3ee23219ed45
Clay Dunston <claydunston@gmail.com>
2022-07-16 10:49:16 -0500

    [client] [style] Import vendor scss code outside of index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,6 @@
 import { render } from "react-dom"
 
-import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle"
-import "keen-slider/keen-slider.min.css"
 
 import { App } from "./ts"
 import "./styles/index.scss"
