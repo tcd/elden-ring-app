90dca18ab65f9a6ffba7095ad83d3a003a5ecf75
Clay Dunston <claydunston@gmail.com>
2022-07-16 09:51:20 -0500

    Fix mobile sidenav & header

--- client/src/ts/features/core/components/mobile-layout/MobileHeader.tsx
+++ client/src/ts/features/core/components/mobile-layout/MobileHeader.tsx
@@ -1,20 +1,37 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 
 import { getImageSrcManual } from "@app/util"
-import { Selectors } from "@app/state"
+import { Actions, Selectors } from "@app/state"
 
 const headerBackgroundSrc = getImageSrcManual("ui/header/400", "public")
 
 export const MobileHeader = (_props: unknown): JSX.Element => {
+
+    const dispatch = useDispatch()
+
     const title = useSelector(Selectors.Core.title)
     const imageUrl = useSelector(Selectors.Core.titleIconUrl)
+    const sideNavOpened = useSelector(Selectors.Core.sideNavOpened)
+
+    const handleIconClick = (_event) => {
+        if (!sideNavOpened) {
+            dispatch(Actions.Core.openSideNav())
+        }
+    }
 
     return (
         <header id="er__appHeader__root">
             <img id="er__appHeader__background" src={headerBackgroundSrc} alt="header background" />
             <div id="er__appHeader__content">
-                <img className="er__appHeader__icon" src={imageUrl} alt={title} />
-                <h1 className="er__appHeader__title">{title}</h1>
+                <img
+                    className="er__appHeader__icon"
+                    src={imageUrl}
+                    alt={title}
+                    onClick={handleIconClick}
+                />
+                <h1 className="er__appHeader__title">
+                    {title}
+                </h1>
             </div>
         </header>
     )
--- client/src/ts/features/core/components/mobile-layout/MobileLayout.tsx
+++ client/src/ts/features/core/components/mobile-layout/MobileLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 
 import { MobileHeader, MobileFooter } from "."
+import { AppSideNav } from "@app/features/core/components/layout/app-side-nav"
 
 export const MobileLayout = () => {
     return (
         <div id="er__mobileLayout">
             <MobileHeader />
             <div id="mobile-outlet-wrapper">
+                <AppSideNav />
                 <Outlet />
             </div>
             <MobileFooter />
