561fe0a7c530d9b548e1269dfa0802ef56011b69
Clay Dunston <claydunston@gmail.com>
2022-07-30 12:31:11 -0500

    Okay, looks like the new deployment works

--- client/config/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source ./config/.env
-
-aws s3 sync ./dist $S3_BUCKET_URI
-
-aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
--- client/config/scripts/deploy.js
+++ client/config/scripts/deploy.js
@@ -1,51 +1,118 @@
+// node
 const { join } = require("path")
-
-const { S3, S3Client } = require("@aws-sdk/client-s3")
+const { writeFile } = require("fs/promises")
+// npm
+const moment = require("moment");
+const { S3Client } = require("@aws-sdk/client-s3")
+const { CloudFrontClient, CreateInvalidationCommand  } = require("@aws-sdk/client-cloudfront");
 const S3SyncClient = require("s3-sync-client")
-
-const { ROOT_FOLDER } = require("./helpers.js")
+// local
 const config = require("../env/env.json")
+const { ROOT_FOLDER } = require("./helpers.js")
 
+// =============================================================================
 
-const distFolderPath = join(ROOT_FOLDER, "dist")
-// console.log(distFolderPath)
-// console.log(env)
-
-
-const listBuckets = async () => {
-    const s3 = new S3({
-        credentials: {
-            accessKeyId: config.defaults.AWS_USER_ACCESS_KEY_ID,
-            secretAccessKey: config.defaults.AWS_USER_SECRET_ACCESS_KEY,
-        }
-    })
-    const response = await s3.listBuckets({});
-    console.log(response)
+/** @returns {string} */
+const getNodeEnv = () => {
+    const nodeEnv = process?.env?.NODE_ENV
+    if (!["production", "staging"].includes(nodeEnv)) {
+        console.error(`invalid environment: '${nodeEnv}'`)
+        process.exit(1)
+    }
+    return nodeEnv
 }
 
-const deployStaging = async () => {
-    const s3Client = new S3Client({
-        credentials: {
-            accessKeyId: config.defaults.AWS_USER_ACCESS_KEY_ID,
-            secretAccessKey: config.defaults.AWS_USER_SECRET_ACCESS_KEY,
+/** @returns {void} */
+const writeLogs = async (timestamp, logsFolder, nodeEnv, s3Output, cloudFrontOutput) => {
+    try {
+        const data = {
+            env: nodeEnv,
+            s3: s3Output,
+            cloudFront: cloudFrontOutput,
         }
-    })
+        const fileContent = JSON.stringify(data, null, 4)
+        const filePath = join(logsFolder, `${nodeEnv}.${timestamp}.json`)
+        await writeFile(filePath, fileContent)
+        console.log(`deployment log written: '${filePath}'`)
+        return null;
+    } catch (error) {
+        console.error("unable to write log file")
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+/**
+ * Sync `client/dist/**` with our S3 bucket on AWS.
+ *
+ * @param {"production" | "staging"} environment
+ * @returns {any}
+ */
+const syncS3 = async (awsConfig, distFolder, environment) => {
+    const s3Client = new S3Client(awsConfig)
     const { sync } = new S3SyncClient({ client: s3Client })
-    const response = await sync(distFolderPath, config.staging.S3_BUCKET_URI, {
+    const response = await sync(distFolder, config[environment].S3_BUCKET_URI, {
         dryRun: false,
     })
-    console.log(response)
+    return response
 }
 
+/**
+ * Create a CloudFront invalidation to clear old cached files.
+ *
+ * @param {"production" | "staging"} environment
+ * @returns {void}
+ */
+ const updateCloudFront = async (awsConfig, timestamp, environment) => {
+    const client = new CloudFrontClient(awsConfig);
+    const command = new CreateInvalidationCommand({
+        DistributionId: config[environment].CLOUDFRONT_DISTRIBUTION_ID,
+        InvalidationBatch: {
+            CallerReference: timestamp,
+            Paths: {
+                Quantity: 1,
+                Items: ["/*"],
+            }
+        },
+    });
+    const response = await client.send(command);
+    return response
+}
 
+/**
+ * @returns {void}
+ */
+const deploy = async () => {
+    /** @type {string} */
+    const nodeEnv = getNodeEnv()
+    /** @type {string} */
+    const timestamp = moment().format("YYYY-MM-DD-HH-mm")
+    /** @type {string} */
+    const distFolderPath = join(ROOT_FOLDER, "dist")
+    /** @type {string} */
+    const logsFolderPath = join(ROOT_FOLDER, "logs", "deployment")
+    /** @type {S3ClientConfig | CloudFrontClientConfig} */
+    const awsConfig = {
+        credentials: {
+            accessKeyId: config.defaults.AWS_USER_ACCESS_KEY_ID,
+            secretAccessKey: config.defaults.AWS_USER_SECRET_ACCESS_KEY,
+        }
+    }
+    console.log("syncing S3")
+    const s3Output = await syncS3(awsConfig, distFolderPath, nodeEnv)
+    console.log("creating CloudFront invalidation")
+    const cloudFrontOutput = await updateCloudFront(awsConfig, timestamp, nodeEnv)
+    await writeLogs(timestamp, logsFolderPath, nodeEnv, s3Output, cloudFrontOutput)
+    return null;
+}
 
+// =============================================================================
 
 const main = async () => {
-    await deployStaging()
+    await deploy()
 }
 
 (async () => {
-    // main()
     main()
         .then((res) => {
             if (res) { console.log(res) }
