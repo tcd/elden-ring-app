673c8ee48974bc1041b796d74183b4cd8d514794
Clay Dunston <claydunston@gmail.com>
2022-08-23 21:35:46 -0500

    Remove some unused code

--- client/src/ts/features/core/components/layout/app-footer/AppFooter.tsx
+++ client/src/ts/features/core/components/layout/app-footer/AppFooter.tsx
@@ -1,34 +1,8 @@
-import { Box, SxProps } from "@mui/material"
-
-// import { AppBottomNav } from "."
 import { AppFooterActions } from "."
 
-const showOnMobile: SxProps = {
-    display: {
-        xs: "inherit",
-        md: "none",
-    },
-}
-
-const showOnDesktop: SxProps = {
-    display: {
-        xs: "none",
-        md: "inherit",
-    },
-}
-
-export const AppFooter = () => {
-
-    // const currentYear = new Date().getFullYear()
-
+export const AppFooter = (_props: unknown): JSX.Element => {
     return (
         <footer id="er__footer">
-            {/* <Box sx={showOnMobile}>
-                <AppBottomNav />
-            </Box> */}
-            {/* <Box sx={showOnDesktop}>
-                <span>Elden Ring - {currentYear}</span>
-            </Box> */}
             <AppFooterActions />
         </footer>
     )
