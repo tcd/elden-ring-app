b0b73fb034508e252309c2fea5a7084bbd92876a
Clay Dunston <claydunston@gmail.com>
2022-08-30 17:23:46 -0500

    Really have to remove Bootstrap.

--- client/src/ts/features/common/components/equipment-menu/Divider.tsx
+++ client/src/ts/features/common/components/equipment-menu/Divider.tsx
@@ -3,5 +3,5 @@ import { Box } from "@mui/material"
 import { ComponentSx } from "@app/theme"
 
 export const EquipmentMenuDivider = (_props: unknown): JSX.Element => {
-    return <Box component="hr" sx={ComponentSx.EquipmentMenu.grid.divider} />
+    return <Box sx={ComponentSx.EquipmentMenu.grid.divider} />
 }
