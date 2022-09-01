8f458e4cf0dc6ad10eab232c8c56cc0fe20459d5
Clay Dunston <claydunston@gmail.com>
2022-09-01 09:25:19 -0500

    Replace `ErCard` with a functional component

--- client/src/ts/features/common/components/elden-ring-material/ErCard/ErCard.tsx
+++ client/src/ts/features/common/components/elden-ring-material/ErCard/ErCard.tsx
@@ -1,16 +1,17 @@
-import { Component, ReactNode } from "react"
-import { Box, SxProps } from "@mui/material"
+import type { SxProps } from "@mui/material"
+import { Box } from "@mui/material"
 
 import { IconNamesKey } from "@app/constants"
-import { ErIcon } from "@app/shared"
-import { isBlank } from "@app/util"
 import { ComponentSx } from "@app/theme"
+import { isBlank } from "@app/util"
+import { ErIcon } from "@app/shared"
 import { ErCardBorder } from "./ErCardBorder"
 
 export interface ErCardProps {
+    children?: React.ReactNode
     title: string
     icon?: IconNamesKey
-    content?: ReactNode
+    content?: React.ReactNode
     /**
      * @default false
      */
@@ -22,66 +23,47 @@ export interface ErCardProps {
     sx?: SxProps
 }
 
-export class ErCard extends Component<ErCardProps> {
-
-    static defaultProps: Partial<ErCardProps> = {
-        icon: null,
-        smallTitle: false,
-        sx: {
-            mx: 3,
-            my: 2,
-        },
-    }
-
-    constructor(props: ErCardProps) {
-        super(props)
-    }
-
-    public render(): JSX.Element {
-        return (
-            <Box component="article" sx={this.sx()}>
-                <Box component="header" sx={ComponentSx.ErCard.header}>
-                    {this.iconElement()}
-                    <Box component="span" sx={this.titleSx()}>
-                        {this.props.title}
-                    </Box>
-                </Box>
-                <Box component="section" sx={ComponentSx.ErCard.content.root}>
-                    <ErCardBorder />
-                    <Box sx={ComponentSx.ErCard.content.content}>
-                        {this?.props?.children && this.props.children}
-                    </Box>
-                </Box>
-            </Box>
-        )
-    }
-
-    // =========================================================================
-
-    private sx(): SxProps {
-        return {
-            ...ErCard.defaultProps.sx,
-            ...(this.props?.sx ?? {}),
-        }
-    }
-
-    private iconElement(): JSX.Element {
-        if (isBlank(this.props.icon)) {
-            return null
-        }
-        return (
-            <ErIcon icon={this.props.icon} />
-        )
-    }
-
-    private titleSx(): SxProps {
-        if (this.props.smallTitle === true) {
-            return ComponentSx.ErCard.title.small
-        }
-        if (isBlank(this.props.icon)) {
-            return ComponentSx.ErCard.title.noIcon
-        }
-        return ComponentSx.ErCard.title.default
-    }
+const defaultProps: Partial<ErCardProps> = {
+    icon: null,
+    smallTitle: false,
+    sx: {
+        mx: 3,
+        my: 2,
+    },
 }
 
+export const ErCard = (props: ErCardProps): JSX.Element => {
+
+    props = { ...defaultProps, ...props }
+
+    return (
+        <Box component="article" sx={props.sx}>
+            <Box component="header" sx={ComponentSx.ErCard.header}>
+                {props?.icon &&
+                    <ErIcon icon={props.icon} />
+                }
+                <Box component="span" sx={titleSx(props)}>
+                    {props.title}
+                </Box>
+            </Box>
+            <Box component="section" sx={ComponentSx.ErCard.content.root}>
+                <ErCardBorder />
+                <Box sx={ComponentSx.ErCard.content.content}>
+                    {props?.children && props.children}
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+// =============================================================================
+
+const titleSx = (props: ErCardProps): SxProps => {
+    if (props.smallTitle === true) {
+        return ComponentSx.ErCard.title.small
+    }
+    if (isBlank(props.icon)) {
+        return ComponentSx.ErCard.title.noIcon
+    }
+    return ComponentSx.ErCard.title.default
+}
