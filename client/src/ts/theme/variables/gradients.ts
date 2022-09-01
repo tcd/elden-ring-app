1729bcd54c6b42f6a31c19710da68525c5b69c53
Clay Dunston <claydunston@gmail.com>
2022-09-01 17:51:43 -0500

    I think that's the last of the gradients for now

--- client/src/ts/theme/variables/gradients.ts
+++ client/src/ts/theme/variables/gradients.ts
@@ -3,25 +3,29 @@ import { gradientStops } from "./gradients-stops"
 import {
     contextMenu,
     equipmentMenu,
+    layout,
     modal,
     startingClass,
     youDied,
 } from "./variables.scss"
 
 export const gradients = {
-    // // Side Nav
-    // sideNav,
-    // sideNavBackdrop,
-    // // Modal
+    // Side Nav
+    sideNav:         linearGradient({ direction: "90deg", color: layout.sideNavBg, stops: gradientStops.sideNav            }),
+    sideNavBackdrop: linearGradient({ direction: "90deg", color: layout.sideNavBg, stops: gradientStops.sideNavBackdrop    }),
+    // Modal
     modalBody:            linearGradient({ direction: "90deg", color: modal.colorBg, stops: gradientStops.modalBody            }),
     modalTopBottomBorder: linearGradient({ direction: "90deg", color: modal.colorBg, stops: gradientStops.modalTopBottomBorder }),
-    // modalInnerBorder,
-    // // Context Menu
-    // contextMenu = modalBody,
+    // modalInnerBorder: [
+    //     linearGradient({ direction: "90deg", color: modal.colorBg, stops: gradientStops.modalInnerBorder1 }),
+    //     linearGradient({ direction: "90deg",                       stops: gradientStops.modalInnerBorder2 }),
+    // ].join(", "),
+    // Context Menu
+    /* contextMenu = modalBody */
     contextMenuItem:       linearGradient({ direction: "90deg", color: contextMenu.item.colorBg,       stops: gradientStops.contextMenuItem }),
     contextMenuItemActive: linearGradient({ direction: "90deg", color: contextMenu.item.colorBgActive, stops: gradientStops.contextMenuItem }),
     // You Died
-    youDied: linearGradient({ direction: "180deg", color: youDied.black,              stops: gradientStops.youDiedGradient     }),
+    youDied: linearGradient({ direction: "180deg", color: youDied.black, stops: gradientStops.youDiedGradient     }),
     // Equipment
     equipmentMenuDivider: linearGradient({ direction: "90deg",  color: equipmentMenu.dividerColor, stops: gradientStops.equipmentGridBorder }),
     equipmentCell:        linearGradient({ direction: "180deg", color: equipmentMenu.cellColor,    stops: gradientStops.equipmentCellBorder }),
