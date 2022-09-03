a887cb3bfe0e135c85a700bcdc0078200a22bb3f
Clay Dunston <claydunston@gmail.com>
2022-09-03 14:49:24 -0500

    misc

--- client/src/ts/theme/variables/breakpoints.ts
+++ client/src/ts/theme/variables/breakpoints.ts
@@ -1,3 +1,11 @@
+const _muiDefaults = {
+    // xs:    0, // extra-small
+    // sm:  600, // small
+    // md:  900, // medium
+    // lg: 1200, // large
+    // xl: 1536, // extra-large
+}
+
 export const gridBreakpoints = {
     xs:     0, // extra-small
     sm:   550, // small
@@ -7,9 +15,9 @@ export const gridBreakpoints = {
     xxl: 1400, // extra-extra-large
 }
 
-export const breakpoints = {
-    mobile:   "550px",
-    tablet:   "770px",
-    laptop:   "990px",
-    desktop: "1200px",
-}
+// export const breakpoints = {
+//     mobile:   "550px",
+//     tablet:   "770px",
+//     laptop:   "990px",
+//     desktop: "1200px",
+// }
