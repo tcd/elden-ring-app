e4135cb3f3d521be2f66c1d975ec9ea0c39800cd
Clay Dunston <claydunston@gmail.com>
2022-09-05 17:08:05 -0500

    Silence `DOMException` when playing audio

--- client/src/ts/util/audio/menu-click.ts
+++ client/src/ts/util/audio/menu-click.ts
@@ -1,12 +1,21 @@
 import { MENU_CLICK } from "@app/data/audio"
 
+import { logger } from "../logging"
+
 /**
  * ## Reference
  *
  * - [HTMLAudioElement](https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement)
  */
-export const menuClick = async () => {
+export const menuClick = async (): Promise<void> => {
     const audioElement = new Audio(MENU_CLICK)
-    await audioElement.play()
-    audioElement.remove()
+    try {
+        await audioElement.play()
+        audioElement?.remove()
+    } catch (error) {
+        audioElement?.remove()
+        logger.info(error)
+    } finally {
+        audioElement?.remove()
+    }
 }
--- client/src/ts/util/audio/slider.ts
+++ client/src/ts/util/audio/slider.ts
@@ -1,12 +1,21 @@
 import { SLIDER_AUDIO } from "@app/data/audio"
 
+import { logger } from "../logging"
+
 /**
  * ## Reference
  *
  * - [HTMLAudioElement](https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement)
  */
-export const slider = async () => {
+export const slider = async (): Promise<void> => {
     const audioElement = new Audio(SLIDER_AUDIO)
-    await audioElement.play()
-    audioElement.remove()
+    try {
+        await audioElement.play()
+        audioElement?.remove()
+    } catch (error) {
+        audioElement?.remove()
+        logger.info(error)
+    } finally {
+        audioElement?.remove()
+    }
 }
