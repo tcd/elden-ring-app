e83fdabf036218aeb7b778783103a1c3d7176c4f
Clay Dunston <claydunston@gmail.com>
2022-09-05 10:34:31 -0500

    Base64 encode favicon

--- client/src/ts/features/core/components/misc/AppHelmet.tsx
+++ client/src/ts/features/core/components/misc/AppHelmet.tsx
@@ -1,17 +1,16 @@
 import { Helmet } from "react-helmet"
+
 import { getImageSrcManual } from "@app/util"
 
 const openGraphTags = [
     { "property": "og:title",        "content": "Elden Ring App" },
     { "property": "og:url",          "content": "https://elden-ring-app.com" },
-    { "property": "og:image",        "content": "https://imagedelivery.net/tIYDWdG54zSW0jZ2i4FVmQ/logo/512" },
+    { "property": "og:image",        "content": getImageSrcManual("logo", "512") },
     { "property": "og:description",  "content": "An Elden Ring build planner and then some" },
     { "property": "og:locale",       "content": "en_US" },
     { "property": "og:site_name",    "content": "Elden Ring App" },
     { "property": "og:type",         "content": "website" },
 ]
-
-const faviconUrl = getImageSrcManual("logo", "128")
 
 export const AppHelmet = (_props: unknown): JSX.Element => {
 
@@ -25,11 +24,6 @@ export const AppHelmet = (_props: unknown): JSX.Element => {
         <Helmet>
             <base/>
             <title>{title}</title>
-            <link
-                rel="icon"
-                type="image/x-icon"
-                href={faviconUrl}
-            />
             {$openGraphTags}
         </Helmet>
     )
