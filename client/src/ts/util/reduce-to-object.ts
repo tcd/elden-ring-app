c82a2a6025fbe9bd58d9b7867cb45bc1f9ad49b5
Clay Dunston <claydunston@gmail.com>
2022-09-08 11:52:17 -0500

    [FMT]

--- client/src/ts/util/reduce-to-object.ts
+++ client/src/ts/util/reduce-to-object.ts
@@ -2,7 +2,14 @@ type RecordKey = string | symbol
 
 type Func<Input, Key extends RecordKey, Result> = (input: Input) => [Key, Result]
 
-export const reduceToObject = <Input, Key extends RecordKey, Result>(array: Input[], reducer: Func<Input, Key, Result>): Record<Key, Result> => {
+export const reduceToObject = <
+    Input,
+    Key extends RecordKey,
+    Result
+    >(
+        array: Input[],
+        reducer: Func<Input, Key, Result>,
+    ): Record<Key, Result> => {
     return array.reduce((accumulated, current) => {
         const [key, result] = reducer(current)
         accumulated[key] = result
