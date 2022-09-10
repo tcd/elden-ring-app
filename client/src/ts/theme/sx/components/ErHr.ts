714a65c9831130037b6ce21d66b64a5dcabc5ab6
Clay Dunston <claydunston@gmail.com>
2022-09-10 18:05:11 -0500

    [WIP] `ErHr`

--- client/src/ts/theme/sx/components/ErHr.ts
+++ client/src/ts/theme/sx/components/ErHr.ts
@@ -5,7 +5,7 @@ import type {
 
 import {
     ThemeVars,
-    ThemeBreakpoints,
+    // ThemeBreakpoints,
 } from "@app/theme"
 
 // =============================================================================
