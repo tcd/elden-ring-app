684eb8bbe1c0a00a21ce942edf81628cebf38238
Clay Dunston <claydunston@gmail.com>
2022-09-10 12:21:06 -0500

    [WIP] fix global mui link props

--- client/src/ts/@types/mui.d.ts
+++ client/src/ts/@types/mui.d.ts
@@ -85,3 +85,14 @@ export declare module "@mui/material/Button" {
         "elden-ring": true
     }
 }
+
+// // =============================================================================
+// // Links
+// // =============================================================================
+//
+// import type { To } from "react-router-dom"
+// import type { LinkTypeMap as MuiLinkTypeMap } from "@mui/material/Link"
+//
+// export declare module "@mui/material/Link" {
+//     export interface LinkTypeMap extends MuiLinkTypeMap<
+// }
