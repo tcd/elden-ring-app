8fdf9e779af740854eab6e6c645a1f4fe5de0250
Clay Dunston <claydunston@gmail.com>
2022-09-12 18:05:09 -0500

    Add eslint plugin for doc comments

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
         "@typescript-eslint",
         "import",
         "lodash",
+        "jsdoc",
     ],
     "settings": {
         "import/internal-regex": "^@app/",
@@ -116,5 +117,14 @@ module.exports = {
             ],
             // "distinctGroup": false,
         }],
+        // ---------------------------------------------------------------------
+        // JsDoc
+        // https://github.com/gajus/eslint-plugin-jsdoc
+        // ---------------------------------------------------------------------
+        "jsdoc/check-alignment":         ["warn"], // https://github.com/gajus/eslint-plugin-jsdoc#eslint-plugin-jsdoc-rules-check-alignment
+        "jsdoc/require-asterisk-prefix": ["warn", "always"], // https://github.com/gajus/eslint-plugin-jsdoc#eslint-plugin-jsdoc-rules-require-asterisk-prefix
+        "jsdoc/multiline-blocks":        ["warn", { "noZeroLineText": true, "noSingleLineBlocks": false }], // https://github.com/gajus/eslint-plugin-jsdoc#eslint-plugin-jsdoc-rules-multiline-blocks
+        "jsdoc/check-line-alignment":    ["off"], // ["warn", "always"],
+        "jsdoc/check-indentation":       ["off"], // https://github.com/gajus/eslint-plugin-jsdoc#eslint-plugin-jsdoc-rules-check-indentation
     },
 }
