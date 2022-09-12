3c03e74f39b94643c68e169042e36db0002e985d
Clay Dunston <claydunston@gmail.com>
2022-09-12 17:57:38 -0500

    [WIP] showcase. probably need to use storybook.

---
+++ client/src/ts/features/showcase/components/ShowcasePage.tsx
@@ -0,0 +1,8 @@
+import { ErPage } from "@app/features/common"
+
+export const ShowcasePage = (_props: unknown): JSX.Element => {
+    return (
+        <ErPage title="Showcase" icon="mausoleum">
+        </ErPage>
+    )
+}
